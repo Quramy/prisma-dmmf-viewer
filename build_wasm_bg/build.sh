056b3235b66de99d8127339b40691a0fb2f24ffe
Quramy <yosuke.kurami@gmail.com>
2023-10-15 22:50:28 +0900

    rename dir

--- build_wasm_bg/build.sh
+++ build_wasm_bg/build.sh
@@ -4,5 +4,5 @@ cp node_modules/@prisma/prisma-schema-wasm/src/prisma_schema_build.js build_wasm
 patch -u build_wasm_bg/prisma_schema_build.js < build_wasm_bg/prisma_schema.js.patch
 cat build_wasm_bg/base.js build_wasm_bg/prisma_schema_build.js > build_wasm_bg/prisma_schema.js
 
-cp build_wasm_bg/prisma_schema.js public
-cp node_modules/@prisma/prisma-schema-wasm/src/prisma_schema_build_bg.wasm public
+cp build_wasm_bg/prisma_schema.js docs
+cp node_modules/@prisma/prisma-schema-wasm/src/prisma_schema_build_bg.wasm docs
